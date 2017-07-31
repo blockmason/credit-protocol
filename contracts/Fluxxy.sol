e2f2518448d4d01297d945c283e2fc84cba4836e
blah <tim.galebach@gmail.com>
2017-07-31 15:36:13 +0000

    new contracts started

---
+++ contracts/Fluxxy.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.11;
+
+/*  Fluxxy
+    The Flux Capacitor
+
+ */
+
+contractFluxxy {
+  /*
+    mirrors the FIDData functions (getters and setters)
+    but also receives a token parameter
+    1. checks Staker to get
+    - user associated with token + contract
+    - total capacity remaining in the time period for the user/token
+    2. if there is capacity remaining
+    - updates capacity in Staker
+    - calls the corresponding function in FIDData
+    3. if no capacity remaining, revert()
+
+   */
+}
