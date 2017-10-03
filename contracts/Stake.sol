67faeb3ba358afdd47e9e757627b22ad2c5abf64
blah <tim.galebach@gmail.com>
2017-10-03 14:50:21 +0000

    Stake added

---
+++ contracts/Stake.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.13;
+
+import "./Adminable.sol";
+import "./StakeData.sol";
+
+contract Stake is Adminable {
+  StakeData sd;
+
+  mapping ( address => address[] ) ucacIdToUcac;
+
+  function Stake(address _stakeDataContract) {
+    sd = StakeData(_stakeDataContract);
+  }
+
+  function ucacAddress(bytes32 ucacId) constant returns (address) {
+    return sd.getUcacAddr(ucacId);
+  }
+
+}
