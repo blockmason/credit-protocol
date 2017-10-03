9b2b6d1c84b6022ba95a3ceabc31e67616c376c5
blah <tim.galebach@gmail.com>
2017-10-03 14:48:21 +0000

    StakeData

---
+++ contracts/StakeData.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.4.15;
+
+import "./Parentable.sol";
+
+contract StakeData is Parentable {
+
+  struct Ucac {
+    address ucacContractAddr;
+    address owner1;
+    address owner2;
+  }
+
+  mapping (bytes32 => Ucac) public ucacs; //indexed by ucacId
+
+  function getUcacAddr(bytes32 _ucacId) public constant returns (address) {
+    return ucacs[_ucacId].ucacContractAddr;
+  }
+
+  function getOwner1(bytes32 _ucacId) public constant returns (address) {
+    return ucacs[_ucacId].owner1;
+  }
+
+  function getOwner2(bytes32 _ucacId) public constant returns (address) {
+    return ucacs[_ucacId].owner2;
+  }
+
+  function isOwner(bytes32 _ucacId, address _owner) public constant returns (bool) {
+    return ucacs[_ucacId].owner1 == _owner || ucacs[_ucacId].owner2 == _owner;
+  }
+
+  function setUcacAddr(bytes32 _ucacId, address _ucacContractAddr) public onlyParent {
+    ucacs[_ucacId].ucacContractAddr = _ucacContractAddr;
+  }
+
+  function setOwner1(bytes32 _ucacId, address _newOwner) public onlyParent {
+    ucacs[_ucacId].owner1 = _newOwner;
+  }
+
+  function setOwner2(bytes32 _ucacId, address _newOwner) public onlyParent {
+    ucacs[_ucacId].owner2 = _newOwner;
+  }
+}
