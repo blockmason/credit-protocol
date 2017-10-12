e2b5dfe7940090b6630fcb2cb9ba3c24ed93f81a
Roy Blankman <riblankman@gmail.com>
2017-10-12 17:55:25 -0400

    full friend signing process working

--- test/FriendCreation.js
+++ test/FriendCreation.js
@@ -12,9 +12,9 @@ const ucacId2 = web3.sha3("yo");
 
 
 const sign = function(signer, content) {
-    // let contentHash = web3.sha3(content);
-    // console.log("content hash: " + contentHash);
-    let sig = web3.eth.sign(signer, content, {encoding: 'hex'});
+    let contentHash = web3.sha3(content, {encoding: 'hex'});
+    console.log("content hash: " + contentHash);
+    let sig = web3.eth.sign(signer, contentHash, {encoding: 'hex'});
     sig = sig.substr(2, sig.length);
 
     let res = {};
@@ -39,9 +39,9 @@ contract('FriendCreationTest', function([p1, p2]) {
     describe("Friend Creation", () => {
         it("allows two parties to sign a message and create a friendship", async function() {
             let content1 = ucacId1 + p2.substr(2, p2.length);
-            let sig1 = sign(p1, ucacId1);
+            let sig1 = sign(p1, content1);
             let content2 = ucacId1 + p1.substr(2, p1.length);
-            let sig2 = sign(p2, ucacId2);
+            let sig2 = sign(p2, content2);
             await this.friendData.initFriendship.sendTransaction(ucacId1, p1, p2, sig1.r, sig1.s, sig1.v, sig2.r, sig2.s, sig2.v, {from: p1});
             let a = await this.friendData.initFriendship(ucacId1, p1, p2, sig1.r, sig1.s, sig1.v, sig2.r, sig2.s, sig2.v, {from: p1});
             assert(a, "signatures are not correct and friendship was not initialized");
